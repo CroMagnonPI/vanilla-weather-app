059069aae761315620a3c6c6ef2cac01d6bd473d
CroMagnonPI <103386186+CroMagnonPI@users.noreply.github.com>
2022-04-10 18:16:35 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours <10){ hours = `0${hours}`}
+    let minutes = date.getMinutes();
+    if (minutes <10){ minutes = `0${minutes}`}
+    let daysofWeek = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    ];
+    let day = daysofWeek[date.getDay()];
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemp(response){
+    let currentTemp = document.querySelector("#current-temp");
+    let city = document.querySelector("#city");
+    let todaysDate = document.querySelector("#date");
+    let description = document.querySelector("#weather-description");
+    let precipitation = document.querySelector("#precipitation");
+    let humidity = document.querySelector("#humidity");
+    let windSpeed = document.querySelector("#wind-speed");
+
+    currentTemp.innerHTML = Math.round(response.data.main.temp);
+    city.innerHTML = response.data.name;
+    todaysDate.innerHTML = formatDate(response.data.dt * 1000)
+    description.innerHTML = response.data.weather[0].description;
+    precipitation.innerHTML = response.data.clouds.all;
+    humidity.innerHTML = response.data.main.humidity;
+    windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "d965f5e3fcb1a2054c7f5ac431fe9773"; 
+let currentCity = "Tacoma";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(displayTemp)
